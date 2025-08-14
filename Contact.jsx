f9e1a642ced1cd37d43834abd7adab05669baa44
mthflashice <matheuslino@kerix.com.br>
2025-08-14 12:05:13 -0300

    create Contact.jsx

---
+++ Contact.jsx
@@ -0,0 +1,23 @@
+const Contact = () => {
+    const whatsappNumber = "5519981293361"; // Seu número com DDD, vou trocar depois
+    const message = "Olá! Gostaria de agendar uma aula de Pilates";
+  
+    return (
+      <section className="contact">
+        <h2>Agende sua Aula</h2>
+        <p>Tire dúvidas ou agende diretamente pelo WhatsApp:</p>
+        
+        <a 
+          href={`https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn-whatsapp"
+        >
+          <i className="whatsapp-icon"></i>
+          Agendar via WhatsApp
+        </a>
+      </section>
+    );
+  };
+  
+  export default Contact;
