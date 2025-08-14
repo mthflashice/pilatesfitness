ddbf9d481ff26a1ddf6eeda38214a1303ad2ad82
mthflashice <matheuslino@kerix.com.br>
2025-08-14 11:36:51 -0300

    create App.jsx

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Schedule from './components/Schedule';
+import Gallery from './components/Gallery';
+import Video from './components/Video';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Hero />
+      <Video />
+      <Schedule />
+      <Gallery />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
