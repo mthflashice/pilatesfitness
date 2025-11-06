41f6a8cf8b03cb833747b38ea25160f605b37f7a
mthflashice <matheuslino@kerix.com.br>
2025-11-06 16:59:48 -0300

    add equipment images to gallery

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './Styles/styless.css'
 import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx';
 import Schedule from './components/Schedule.jsx';
-import Gallery from './components/Gallery.jsx';
+import EquipmentGallery from './components/EquipmentGallery.jsx';
 import Video from './components/Video.jsx';
 import Contact from './components/Contact.jsx';
 
@@ -13,7 +13,7 @@ function App() {
       <Hero />
       <Video />
       <Schedule />
-      <Gallery />
+      <EquipmentGallery/>
       <Contact />
     </div>
   );
